7c3123e95815e9836f7a832c8b897cd76e79a4e2
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2023-08-13 16:18:13 +0600

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Red Locker | Hotel Booking</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
     <!-- jQuery UI CSS -->
-    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="{{asset('assets/css/jquery-ui.css')}}">
     <!-- Style CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
     <!-- Responsive CSS -->
-    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="{{asset('assets/css/responsive.css')}}">
 </head>
 <body>
 
@@ -24,15 +24,15 @@
     @include('layouts.includes.footer')
 
     <!-- jQuery Library -->
-    <script src="assets/js/jquery.min.js"></script>
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
     <!-- Bootstrap Bundle JS -->
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
     <!-- jQuery Easing -->
-    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="{{asset('assets/js/jquery.easing.min.js')}}"></script>
     <!-- jQuery UI -->
-    <script src="assets/js/jquery-ui.js"></script>
+    <script src="{{asset('assets/js/jquery-ui.js')}}"></script>
     <!-- jQuery Main -->
-    <script src="assets/js/main.js"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
 </body>
 </html>
 
