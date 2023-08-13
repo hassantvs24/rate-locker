5f28fea81cead6ac832de308800f25b0b03376e0
Nazmul Hossain <49582324+hassantvs24@users.noreply.github.com>
2023-08-13 18:38:47 +0600

    Update header.blade.php

--- resources/views/layouts/includes/header.blade.php
+++ resources/views/layouts/includes/header.blade.php
@@ -3,14 +3,14 @@
         <div class="row align-items-end">
             <div class="col-xl-8">
                 <nav class="navbar navbar-expand-xl align-items-end">
-                    <a class="navbar-brand" href="#"><img src="{{asset('assets/img/logo.png')}}" alt=""></a>
+                    <a class="navbar-brand" href="{{route('home')}}"><img src="{{asset('assets/img/logo.png')}}" alt=""></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                         <ul class="navbar-nav me-auto menu">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Home</a>
+                                <a class="nav-link" href="{{route('home')}}">Home</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Deals</a>
